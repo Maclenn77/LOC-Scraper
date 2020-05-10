568df183ac80aadec080a5a08e7a70ef8bad8a5e
Maclenn77 <maclenn77@gmail.com>
2020-05-10 08:10:45 -0500

    Improve UI. User can check inside sets description and url

--- bin/main.rb
+++ bin/main.rb
@@ -53,17 +53,18 @@ def display_set(object)
 end
 
 def check_image(option, set)
-  if option = 'a'
-    until choice == 0
+  choice = 1
+  if option == 'a'
+    until choice.negative?
       puts "Enter a number (from 1 to #{set.total_images} or 'exit')"
       choice = gets.chomp.to_i
       choice -= 1
       bar_start
-      puts ' ' * 10 + "- Description: #{set.elements[choice][0]}"
+      puts "DESCRIPTION: #{set.elements[choice][0]}"
       bar_start
     end
   elsif option == 'b'
-    until choice == 0
+    until choice.negative?
       puts "Enter a number (from 1 to #{set.total_images} or 'exit')"
       choice = gets.chomp.to_i
       choice -= 1
@@ -72,30 +73,36 @@ def check_image(option, set)
       bar_start
     end
   elsif option == 'c'
-    until choice == 0
+    until choice.negative?
       puts "Enter a number (from 1 to #{set.total_images} or 'exit')"
       choice = gets.chomp.to_i
       choice -= 1
       bar_start
-      puts ' ' * 10 + "- Url: #{set.elements[choice][0]}"
+      puts "DESCRIPTION: #{set.elements[choice][0]}"
       puts ' ' * 10 + "- Url: #{set.elements[choice][1]}"
       bar_start
     end
+  elsif option == 'd'
+    img_range = (1...set.total_images)
+    img_range.each do |i|
+      puts '--'.center(80, '-')
+      puts "Image #{i}. \n  Description: #{set.elements[i][0]} \n Url: #{set.elements[i][1]}"
+    end
   else
-
-    puts
+    puts '#### Invalid option ####'.center(80, ' ')
+  end
 end
 
 def explore_set(answer, set)
   if answer == 'y'
     puts '>> What do you want to do?'
-    puts 'a) Check a description b) Check a url c) Check all images'.center(80, ' ')
+    puts 'a) Check a description b) Check an url c) Check description and url d) Check all images'.center(80, ' ')
     option = gets.chomp.downcase
+    check_image(option, set)
   else
     puts 'Displaying menu again'
   end
 end
-
 
 # Start user interface
 bar_start
@@ -123,7 +130,9 @@ while set_menu == true
     choice = gets.chomp
     set = ImgSet.new(choice, img_sets)
     display_set(set)
-    puts 'Do you want to explore the set?'
+    puts 'Do you want to explore the set? (Y/N)'
+    answer = gets.chomp.downcase
+    explore_set(answer, set)
   elsif choice == 'b'
     set_menu = false
   else

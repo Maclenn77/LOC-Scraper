548e828c3fb120231b092af4dbefa83c8bcaf02a
Maclenn77 <maclenn77@gmail.com>
2020-05-11 02:45:21 -0500

    Created methods for displaying description and url of sets

--- lib/imgsets.rb
+++ lib/imgsets.rb
@@ -15,4 +15,26 @@ class ImgSet < SetsCreator
                      'https://www.loc.gov/free-to-use-/horses' + doc.xpath('//figure//img')[i].attribute('src').value
     end
   end
+
+  def display_description(index)
+    description = "DESCRIPTION: #{@elements[index][0]}"
+    description
+  end
+
+  def display_url(index)
+    url = "URL: #{@elements[index][1]}"
+    url
+  end
+
+  def display_url_nd_description(index)
+    description_plus_url = "#{display_description(index)} \n #{display_url(index)} \n"
+    description_plus_url
+  end
+
+  def all_items
+    display_items = "ALL ITEMS \n"
+    img_range = (1...total_images)
+    img_range.each { |i| display_items << ("Image #{i}. \n" + display_url_nd_description(i)) }
+    display_items
+  end
 end
